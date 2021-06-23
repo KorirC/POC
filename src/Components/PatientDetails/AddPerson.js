6c4ff96c020d676d00140a4ffa1ced3dc9c6130c
cynthia chebet <chebetcynthia8@gmail.com>
2021-06-23 09:46:28 +0300

    function to create person

---
+++ src/Components/PatientDetails/AddPerson.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import apiURL from "../API/Config";
+
+const user = sessionStorage.getItem("user");
+
+const AddPerson = async (data) => {
+  let config = {
+    method: "POST",
+    url: apiURL + "person",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${user}`,
+    },
+    data: data,
+    json: true,
+  };
+
+  try {
+    return axios(config)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { AddPerson };
