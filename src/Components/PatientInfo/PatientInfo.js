05cf1967c743351322ebe0ce4c33208b63af24c3
cynthia chebet <chebetcynthia8@gmail.com>
2021-06-29 16:31:34 +0300

    the sort function by encounterDatetime

--- src/Components/PatientInfo/PatientInfo.js
+++ src/Components/PatientInfo/PatientInfo.js
@@ -10,11 +10,9 @@ import {
   TableHeader,
   TableBody,
   TableCell,
-  Pagination,
 } from "carbon-components-react";
 import { PatientEncounters } from "../Encounters/PatientEncounters";
-
-const _ = require("lodash");
+import PagePagination from "../Pagination/Pagination";
 const moment = require("moment");
 
 const headers = [
@@ -26,95 +24,97 @@ const PatientInfo = (props) => {
   const [encounters, setEncounters] = useState([]);
   const [firstRowIndex, setFirstRowIndex] = useState(0);
   const [currentPageSize, setCurrentPageSize] = useState(5);
+  // const { match: { params } } = props;
   const id = props.match.params.id;
+
   useEffect(() => {
-   
     PatientEncounters(id).then((resp) => {
       const results = resp.map((patient) => {
         return {
           id: patient.uuid,
           uuid: patient.uuid,
           display: patient.display,
-          encounterDatetime: moment(patient.encounterDatetime).format("DD/MM/YYYY"),
+          // encounterDatetime: moment(patient.encounterDatetime).format("DD/MM/YYYY"),
+          encounterDatetime: patient.encounterDatetime,
           location: patient.location.description,
         };
       });
-      // _.sortBy(results, (encounter) => encounter.encounterDatetime);
-      _.reverse(results);
+
+      // console.log(results);
+      results.sort(function compare(a, b) {
+        let dateA = new moment(a.encounterDatetime).format("YYYYMMDD");
+        let dateB = new moment(b.encounterDatetime).format("YYYYMMDD");
+        return dateB - dateA;
+      });
+
       setEncounters(results);
     });
-  },[id]);
+  }, [id]);
 
-  return (
-    <div className="bx--grid--full-width">
-      <Row>
-        <div className="bx--col-lg-2"></div>
-        <div className="bx--col-lg-12" id="patientInfoForm">
-          <div id="patientinfo">
-            <DataTable
-              rows={encounters.slice(
-                firstRowIndex,
-                firstRowIndex + currentPageSize
-              )}
-              headers={headers}
-              isSortable
-            >
-              {({ rows, headers, getHeaderProps, getTableProps }) => (
-                <TableContainer title="Patient Encounters">
-                  <Table {...getTableProps()}>
-                    <TableHead>
-                      <TableRow>
-                        {headers.map((header) => (
-                          <TableHeader {...getHeaderProps({ header })}>
-                            {header.header}
-                          </TableHeader>
-                        ))}
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {rows.length === 0 ? (
+ 
+    return (
+      <div className="bx--grid--full-width">
+        <Row>
+          <div className="bx--col-lg-2"></div>
+          <div className="bx--col-lg-12" id="patientInfoForm">
+            <div id="patientinfo">
+              <DataTable
+                rows={encounters.slice(
+                  firstRowIndex,
+                  firstRowIndex + currentPageSize
+                )}
+                headers={headers}
+                isSortable
+              >
+                {({ rows, headers, getHeaderProps, getTableProps }) => (
+                  <TableContainer title="Patient Encounters">
+                    <Table {...getTableProps()} useZebraStyles>
+                      <TableHead>
                         <TableRow>
-                          <TableCell colSpan="8">
-                            <h5>No Records Found</h5>
-                          </TableCell>
+                          {headers.map((header) => (
+                            <TableHeader {...getHeaderProps({ header })}>
+                              {header.header}
+                            </TableHeader>
+                          ))}
                         </TableRow>
-                      ) : (
-                        rows.map((row) => (
-                          <TableRow key={row.id}>
-                            {row.cells.map((cell) => (
-                              <TableCell key={cell.id}>{cell.value}</TableCell>
-                            ))}
+                      </TableHead>
+                      <TableBody>
+                        {rows.length === 0 ? (
+                          <TableRow>
+                            <TableCell colSpan="8">
+                              <h5>No Records Found</h5>
+                            </TableCell>
                           </TableRow>
-                        ))
-                      )}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              )}
-            </DataTable>
-            <div style={{ width: "100%" }}>
-              <Pagination
-                backwardText="Previous page"
-                forwardText="Next page"
-                itemsPerPageText="Items per page:"
-                pageNumberText="Page Number" 
-                pageSize={currentPageSize}
-                pageSizes={[5, 10, 15, 20, 25]}
-                totalItems={encounters.length}
-                onChange={({ page, pageSize }) => {
-                  if (pageSize !== currentPageSize) {
-                    setCurrentPageSize(pageSize);
-                  }
-                  setFirstRowIndex(pageSize * (page - 1));
-                }}
-              />
+                        ) : (
+                          rows.map((row) => (
+                            <TableRow key={row.id}>
+                              {row.cells.map((cell) => (
+                                <TableCell key={cell.id}>
+                                  {cell.value}
+                                </TableCell>
+                              ))}
+                            </TableRow>
+                          ))
+                        )}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                )}
+              </DataTable>
+              <div style={{ width: "100%" }}>
+                <PagePagination
+                  totalItems={encounters.length}
+                  setFirstRowIndex={setFirstRowIndex}
+                  setCurrentPageSize={setCurrentPageSize}
+                  currentPageSize={currentPageSize}
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="bx--col-lg-2"></div>
-      </Row>
-    </div>
-  );
+          <div className="bx--col-lg-2"></div>
+        </Row>
+      </div>
+    );
 };
 export default PatientInfo;
