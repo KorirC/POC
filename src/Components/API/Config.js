7682871405d1ef077199277e92ed08674f0cdd55
cynthia chebet <chebetcynthia8@gmail.com>
2021-07-02 15:03:27 +0300

    chnaged the url to openmrs

--- src/Components/API/Config.js
+++ src/Components/API/Config.js
@@ -1,7 +1,7 @@
-const apiURL ="http://10.50.80.115:8090/amrs/ws/rest/v1/";
+const apiURL ="/https://openmrs-spa.org/openmrs/ws/rest/v1/";
 export default apiURL;
 
 //http://10.50.80.115:8090/amrs/ws/rest/v1/
 //http://10.50.80.115:8091/
 //https://kibana.ampath.or.ke/amrs/
-
+//https://openmrs-spa.org/openmrs/ws/rest/v1/
