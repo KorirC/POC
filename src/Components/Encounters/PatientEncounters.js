aac49f0eca0c249e9c352ae32a9dbb2701a16423
cynthia chebet <chebetcynthia8@gmail.com>
2021-07-02 15:07:59 +0300

    used return console in the catch function

--- src/Components/Encounters/PatientEncounters.js
+++ src/Components/Encounters/PatientEncounters.js
@@ -2,16 +2,16 @@ import axios from "axios";
 import AuthHeaders from "../API/AuthHeaders";
 import apiURL from "../API/Config";
 
-const PatientEncounters =  async (id) => {
+const PatientEncounters = async (id) => {
   try {
     const res = await axios.get(
       apiURL +
-      `/encounter?patient=${id}&v=custom:(uuid,display,encounterDatetime,location)`,
+        `/encounter?patient=${id}&v=custom:(uuid,display,encounterDatetime,location)`,
       AuthHeaders
     );
     return res.data.results;
   } catch (error) {
-    console.log(error);
+    return console.log(error);
   }
 };
 export { PatientEncounters };
