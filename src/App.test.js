6a11e317d0865e9c790f1021b17dafab70e663b8
cynthia chebet <chebetcynthia8@gmail.com>
2021-07-02 15:02:41 +0300

    test unit for the application

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-// import { render } from '@testing-library/react';
-// import App from './App';
+import { render,screen } from '@testing-library/react';
+import App from './App';
+import Login from "./Components/Login/login";
+import PatientInfo from "./Components/PatientInfo/PatientInfo";
+import { MemoryRouter } from 'react-router';
 
-// test('renders learn react link', () => {
-//   // render(<App />);
-//   // const linkElement = screen.getByText(/learn react/i);
-//   // expect(linkElement).toBeInTheDocument();
-// });
+jest.mock("./Components/Login/login");
+jest.mock("./Components/PatientInfo/PatientInfo");
+test('renders learn react link', () => {
+  // render(<App />);
+  Login.mockImplementation(()=><div>Login</div>);
+  PatientInfo.mockImplementation(()=><div>PatientInfo</div>);  
+  const component = render(
+    <MemoryRouter>
+      <App/>
+    </MemoryRouter>
+  );
+   
+  const linkElement = screen.getByText(/Logout/i);
+  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Login/i)).toBeInTheDocument();
+  // expect(component).toHaveLength(1);
+});
